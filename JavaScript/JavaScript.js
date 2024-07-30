77ca4469fdfb1a3184b32d42164bbe7fbb538510
KUYU <134703137+MCPRniconico@users.noreply.github.com>
2024-07-30 17:45:19 +0900

    Update JavaScript.js

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,165 +1,188 @@
-const { Client, GatewayIntentBits, Events, REST, Routes, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, PermissionsBitField, InteractionType } = require('discord.js');
-const { token } = require('./config.json'); // Tokenを含むconfig.jsonを用意してください
+const { Client, GatewayIntentBits, Partials, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const token = 'YOUR_BOT_TOKEN';
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageContent] });
-const rest = new REST({ version: '10' }).setToken(token);
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ],
+    partials: [Partials.Channel]
+});
+
+client.once('ready', () => {
+    console.log('Logged in!');
+    client.user.setPresence({ activities: [{ name: 'Investigating bugs' }], status: 'online' });
+    const rest = new REST({ version: '9' }).setToken(token);
+    rest.put(
+        Routes.applicationCommands(client.user.id),
+        { body: commands }
+    )
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
+});
 
 const commands = [
     {
         name: 'call',
-        description: 'scpsl募集します指定のサーバーのみ機能します'
+        description: 'Recruit for SCP:SL, only works on specified servers'
     },
     {
         name: 'info',
-        description: 'BOTの情報を表示します'
+        description: 'Displays bot information'
     },
     {
         name: 'emoji',
-        description: 'BOTに搭載されている絵文字をすべて表示します'
+        description: 'Displays all emojis installed in the bot'
     },
     {
         name: 'botid',
-        description: 'BOTのIDなどの情報を表示します'
+        description: 'Displays bot ID information'
     },
     {
         name: 'oplist',
-        description: 'サーバー運営情報を表示します'
+        description: 'Displays server operation information'
     },
     {
         name: 'ticket',
-        description: 'チケットを作成',
-        defaultPermission: false
+        description: 'Create a ticket',
+        default_permission: false
+    },
+    {
+        name: 'embed',
+        description: 'Displays operational partners'
     }
 ];
 
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
-
-        await rest.put(Routes.applicationCommands(client.user.id), { body: commands });
-
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
-})();
-
-client.once(Events.ClientReady, () => {
-    console.log('ログインしました');
-    client.user.setPresence({
-        activities: [{ name: 'BETA版です', type: 0 }], // 0 is PLAYING
-        status: 'online'
-    });
-});
-
-client.on(Events.InteractionCreate, async (interaction) => {
-    if (interaction.type !== InteractionType.ApplicationCommand) return;
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
 
     const { commandName } = interaction;
 
     if (commandName === 'call') {
-        await interaction.reply('<:mukou:1267467191142322216> エラー');
+        await interaction.reply('<:__:1267673604020109452>エラー');
     } else if (commandName === 'info') {
-        await interaction.reply({
-            content: '開発者<:DEV:1267440009452064829><@1002132268736856136> 開発協力者<:ACCDEV:1267440500625903618> <@1032649313165258772> バージョン<:BE:1267439343882993734><:TA:1267439331069657119>v1',
-            ephemeral: true
-        });
+        await interaction.reply('開発者<:DEV:1267440009452064829><@1002132268736856136> 開発協力者<:ACCDEV:1267440500625903618> <@1032649313165258772> バージョン<:BE:1267439343882993734><:TA:1267439331069657119>v1', { ephemeral: true });
     } else if (commandName === 'emoji') {
-        await interaction.reply({
-            content: '搭載絵文字 <:partner:1267440895037542471> <:ACCDEV:1267440500625903618> <:DEV:1267440009452064829> <:BE:1267439343882993734> <:TA:1267439331069657119> <:B_:1267438603718627378> <:mukou:1267467191142322216>',
-            ephemeral: true
-        });
+        await interaction.reply('搭載絵文字 <:partner:1267440895037542471> <:ACCDEV:1267440500625903618> <:DEV:1267440009452064829> <:BE:1267439343882993734> <:TA:1267439331069657119> <:B_:1267438603718627378> <:mukou:1267467191142322216>', { ephemeral: true });
     } else if (commandName === 'botid') {
-        await interaction.reply({
-            content: '<:mukou:1267467191142322216>このコマンドを実行する権限がありません',
-            ephemeral: true
-        });
+        await interaction.reply('<:mukou:1267467191142322216>このコマンドを実行する権限がありません ', { ephemeral: true });
     } else if (commandName === 'oplist') {
-        await interaction.reply({
-            content: '<:mukou:1267467191142322216> NODATA',
-            ephemeral: true
-        });
+        await interaction.reply('<:mukou:1267467191142322216> NODATA', { ephemeral: true });
     } else if (commandName === 'ticket') {
-        if (!interaction.memberPermissions.has(PermissionsBitField.Flags.Administrator)) {
-            await interaction.reply({
-                content: 'このコマンドを実行する権限がありません。',
-                ephemeral: true
-            });
-            return;
-        }
-
-        const channel = interaction.channel;
-
+        const channel = await interaction.guild.channels.cache.get(interaction.channelId);
         const embed = new EmbedBuilder()
             .setColor(0x178CE6)
-            .setDescription("お問い合わせはこちらからチケットを発行してください！")
-            .setFooter({ text: "Made by Spicy │2024/07/30" });
-
+            .setFooter({ text: 'Made by Spicy │2024/07/30' })
+            .addFields({ name: 'チケット', value: 'お問い合わせはこちらからチケットを発行してください！' });
         await channel.send({ embeds: [embed] });
 
-        const button = new ButtonBuilder()
-            .setCustomId('create_ticket')
-            .setLabel('チケットを作成')
-            .setStyle(ButtonStyle.Primary);
-
-        const row = new ActionRowBuilder().addComponents(button);
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('create_ticket')
+                    .setLabel('チケットを作成')
+                    .setStyle(ButtonStyle.Primary),
+            );
 
         await interaction.reply({ content: '', components: [row] });
+    } else if (commandName === 'embed') {
+        const embed = new EmbedBuilder()
+            .setTitle('リスト')
+            .setDescription('<:DEV:1267440009452064829>所有者<@1002132268736856136>　STEAMID:76561199555791158')
+            .setColor(0x00ff00)
+            .addFields(
+                { name: 'EVENTManager', value: '<@1189153844043722753> STEAMID 76561199525003326', inline: false },
+                { name: 'EVENTManager', value: 'NotData', inline: true },
+                { name: 'moderator', value: 'NotDATA。', inline: false },
+                { name: '<:1109542935038545960:1267745305214849035>Trusted', value: '<@1135069041569054750>', inline: true },
+                { name: '<:1109542935038545960:1267745305214849035>Trusted', value: '<@1104907457597296762>', inline: false },
+                { name: '<:1109542935038545960:1267745305214849035>Trusted', value: '<@716212058445709362>', inline: true },
+                { name: '<:1109542935038545960:1267745305214849035>Trusted', value: '<@1234473338706067486>', inline: false },
+                { name: '<:1109542935038545960:1267745305214849035>Trusted', value: '<@1085931921202229259>', inline: true },
+                { name: '<:partner:1267440895037542471>YT', value: '<@1101479370197061672>。', inline: false },
+                { name: '<:partner:1267440895037542471>YT', value: '<@1020505405769666621>。', inline: true },
+                { name: '<:partner:1267440895037542471>YT', value: '<@1039717417116512327>。', inline: false },
+                { name: '<:partner:1267440895037542471>YT', value: '<@463867826676039690>。', inline: true },
+                { name: '<:partner:1267440895037542471>YT', value: '<@1020505405769666621>。', inline: false },
+                { name: '<:partner:1267440895037542471>YT', value: '<@297766775653924865>。', inline: true },
+                { name: '<:partner:1267440895037542471>YT', value: '<@1002132268736856136>。', inline: false },
+                { name: '<:partner2:1267748273502818395>パートナー', value: '<@1101479370197061672>。', inline: true },
+                { name: 'DIscordr', value: 'Notdata。', inline: false }
+            )
+            .setFooter({ text: '2024/7/30' })
+            .setThumbnail('https://example.com/thumbnail.jpg')
+            .setImage('https://example.com/image.jpg');
+
+        await interaction.reply({ embeds: [embed], ephemeral: true });
     }
 });
 
-const ticketOwners = new Map();
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isButton()) return;
 
-client.on(Events.InteractionCreate, async (interaction) => {
-    if (interaction.type !== InteractionType.MessageComponent) return;
-
-    const customId = interaction.customId;
+    const { customId } = interaction;
 
     if (customId === 'create_ticket') {
-        const guild = interaction.guild;
+        const server = interaction.guild;
 
-        if (ticketOwners.has(interaction.user.id)) {
+        if (Object.values(ticketOwners).includes(interaction.user.id)) {
             await interaction.reply({ content: '既にチケットが存在します。', ephemeral: true });
             return;
         }
 
-        const channelName = `チケット-${interaction.user.username}`;
         const overwrites = [
             {
-                id: guild.id,
-                deny: [PermissionsBitField.Flags.ViewChannel],
+                id: server.roles.everyone.id,
+                deny: ['ViewChannel']
             },
             {
-                id: interaction.guild.me.id,
-                allow: [PermissionsBitField.Flags.ViewChannel],
+                id: client.user.id,
+                allow: ['ViewChannel']
             },
             {
                 id: interaction.user.id,
-                allow: [PermissionsBitField.Flags.ViewChannel],
-            },
+                allow: ['ViewChannel']
+            }
         ];
 
-        const channel = await guild.channels.create({ name: channelName, type: 0, permissionOverwrites: overwrites });
+        const channelName = `チケット-${interaction.user.username}`;
+        const channel = await server.channels.create(channelName, {
+            type: 0,
+            permissionOverwrites: overwrites
+        });
 
-        ticketOwners.set(interaction.user.id, channel.id);
+        ticketOwners[channel.id] = interaction.user.id;
 
-        await channel.send(`チケットが作成されました！ ${interaction.user}`);
-        const deleteButton = new ButtonBuilder()
-            .setCustomId('delete_ticket')
-            .setLabel('チケットを削除')
-            .setStyle(ButtonStyle.Danger);
+        await channel.send(`${interaction.user} チケットが作成されました!`);
 
-        const row = new ActionRowBuilder().addComponents(deleteButton);
+        const ticketMessage = `チケットが作成されました！\n${channel}`;
 
-        await channel.send({ components: [row] });
+        await interaction.reply({ content: ticketMessage, ephemeral: true });
 
-        await interaction.reply({ content: `チケットが作成されました！ ${channel}`, ephemeral: true });
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('delete_ticket')
+                    .setLabel('チケットを削除')
+                    .setStyle(ButtonStyle.Danger),
+            );
+
+        await channel.send({ content: '', components: [row] });
     } else if (customId === 'delete_ticket') {
         const channel = interaction.channel;
 
         await channel.delete();
-        ticketOwners.delete(interaction.user.id);
+
+        if (ticketOwners[channel.id]) {
+            delete ticketOwners[channel.id];
+        }
     }
 });
 
 client.login(token);
+
+const ticketOwners = {};
